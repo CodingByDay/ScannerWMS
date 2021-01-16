879acd4f4f3ff81c520f2e4a78828ab12be177ae
hanumansitaram <hanuman@unv.rs>
2021-01-16 14:30:50 +0100

    Adding next view

--- ScannerQR/TakeOverEnteredPositionsView.cs
+++ ScannerQR/TakeOverEnteredPositionsView.cs
@@ -9,27 +9,93 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using TrendNET.WMS.Core.Data;
+using TrendNET.WMS.Device.App;
+using TrendNET.WMS.Device.Services;
 
 namespace ScannerQR
 {
     [Activity(Label = "TakeOverEnteredPositionsView")]
     public class TakeOverEnteredPositionsView : Activity
     {
+        private TextView lbInfo;
+
+        private EditText tbIdent; 
+        private EditText tbSSCC; 
+        private EditText tbSerialNumber; 
+        private EditText tbQty; 
+        private EditText tbLocation; 
+        private EditText tbCreatedBy;
+
+        private Button btNext;
+        private Button btUpdate;
+        private Button button4;
+        private Button btFinish;
+        private Button btDelete;
+        private Button button5;
+        private int displayedPosition = 0;
+        private NameValueObject moveHead = (NameValueObject)InUseObjects.Get("MoveHead");
+        private NameValueObjectList positions = null;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            // Create your application here
+            // Create your application 
+
+
             SetContentView(Resource.Layout.TakeOverEnteredPositionsView);
 
-            Button button = FindViewById<Button>(Resource.Id.button2);
+            lbInfo = FindViewById<TextView>(Resource.Id.lbInfo);
+            tbIdent = FindViewById<EditText>(Resource.Id.tbIdent);
+            tbSSCC = FindViewById<EditText>(Resource.Id.tbSSCC);
+            tbSerialNumber = FindViewById<EditText>(Resource.Id.tbSerialNum);
+            tbQty = FindViewById<EditText>(Resource.Id.tbQty);
+            tbLocation = FindViewById<EditText>(Resource.Id.tbLocation);
+            tbCreatedBy = FindViewById<EditText>(Resource.Id.tbCreatedBy);
 
-            button.Click += Button_Click;
+            btNext = FindViewById<Button>(Resource.Id.btNext);
+            btUpdate = FindViewById<Button>(Resource.Id.btUpdate);
+            button4 = FindViewById<Button>(Resource.Id.button4);
+            btFinish = FindViewById<Button>(Resource.Id.btFinish);
+
+
+            InUseObjects.ClearExcept(new string[] { "MoveHead" });
+            if (moveHead == null) { throw new ApplicationException("moveHead not known at this point!?"); }
+
+            LoadPositions();
         }
-
-        private void Button_Click(object sender, EventArgs e)
+        private void LoadPositions()
         {
-            StartActivity(typeof(TakeOverIdentEntry));
+           
+            try
+            {
+              
+                if (positions == null)
+                {
+                    var error = "";
+
+                    if (positions == null)
+                    {
+                        positions = Services.GetObjectList("mi", out error, moveHead.GetInt("HeadID").ToString());
+                        InUseObjects.Set("TakeOverEnteredPositions", positions);
+                    }
+                    if (positions == null)
+                    {
+                        Toast.MakeText(this, "Napaka pri dostopu do web aplikacije: " + error, ToastLength.Long).Show();
+                        Program.Exit(() => { MessageForm.Show("Napaka pri dostopu do web aplikacije: " + error); });
+                        return;
+                    }
+                }
+
+                displayedPosition = 0;
+                FillDisplayedItem();
+            }
+            finally
+            {
+                wf.Stop();
+            }
         }
+
     }
 }
