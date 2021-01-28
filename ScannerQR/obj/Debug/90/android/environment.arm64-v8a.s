77cecfc036489fbccd9c2ff0f479cc5fb529ed7c
hanumansitaram <hanuman@unv.rs>
2021-01-28 10:12:28 +0100

    Adding the isTablet method

--- ScannerQR/obj/Debug/90/android/environment.arm64-v8a.s
+++ ScannerQR/obj/Debug/90/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"f5523ac4-4571-4c63-a8fa-e671a56ec370"
+	.asciz	"757a21f1-d517-47be-96e7-8701db3fbb84"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
