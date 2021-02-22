6e7849ebfde97c30c93b3facc36c71a6cf9c039e
Janko <jankojovicic351@gmail.com>
2021-02-22 08:16:55 +0100

    Adding the new adapter

---
+++ ScannerQR/App/TakeOverEnteredPositionsViewAdapter.cs
@@ -0,0 +1,95 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScannerQR.App
+{
+    class TakeOverEnteredPositionsViewAdapter : BaseAdapter
+    {
+        public List<TakeOverEnteredPositionsViewListItems> sList;
+        private Context sContext;
+        public TakeOverEnteredPositionsViewAdapter(Context context, List<TakeOverEnteredPositionsViewListItems> list)
+        {
+            sList = list;
+            sContext = context;
+        }
+
+
+
+        public override int Count
+        {
+            get
+            {
+                return sList.Count;
+            }
+        }
+
+        public override Java.Lang.Object GetItem(int position)
+        {
+            return null;
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            View row = convertView;
+            try
+            {
+                if (row == null)
+                {
+                    row = LayoutInflater.From(sContext).Inflate(Resource.Layout.TakeOverEnteredPositionsViewAdapterView, null, false);
+                }
+
+                TextView Ident = row.FindViewById<TextView>(Resource.Id.Ident);
+                Ident.Text = sList[position].Ident;
+
+
+                TextView Name = row.FindViewById<TextView>(Resource.Id.Name);
+                Name.Text = sList[position].Name;
+
+                TextView Quantity = row.FindViewById<TextView>(Resource.Id.Quantity);
+                Quantity.Text = sList[position].Quantity;
+
+
+                TextView Position = row.FindViewById<TextView>(Resource.Id.Position);
+                Position.Text = sList[position].Position;
+
+                TextView SerialNumber = row.FindViewById<TextView>(Resource.Id.SerialNumber);
+                SerialNumber.Text = sList[position].SerialNumber;
+
+                TextView SSCC = row.FindViewById<TextView>(Resource.Id.SSCC);
+                SSCC.Text = sList[position].SSCC;
+
+
+
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+            }
+            finally { }
+
+            return row;
+
+        }
+
+
+        public override void NotifyDataSetChanged()
+        {
+
+
+            NotifyDataSetChanged();
+        }
+
+    }
+}
