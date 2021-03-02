8b56f8384353497912a43bbe3a3d7a804d3bc4ef
Janko <jankojovicic351@gmail.com>
2021-03-02 13:54:36 +0100

    Adding the added RapidTakeover

--- ScannerQR/RapidTakeover.cs
+++ ScannerQR/RapidTakeover.cs
@@ -11,6 +11,7 @@ using System.Text;
 using TrendNET.WMS.Core.Data;
 using TrendNET.WMS.Device.App;
 using TrendNET.WMS.Device.Services;
+using WebApp = TrendNET.WMS.Device.Services.WebApp;
 
 namespace ScannerQR
 {
@@ -26,6 +27,9 @@ namespace ScannerQR
         private List<ComboBoxItem> data = new List<ComboBoxItem>();
         private EditText tbReceiveLocation;
         private EditText tbRealStock;
+        public static NameValueObject dataItem;
+        private NameValueObject moveHead = (NameValueObject)InUseObjects.Get("MoveHead");
+        private NameValueObject moveItem = (NameValueObject)InUseObjects.Get("MoveItem");
         public void GetBarcode(string barcode)
         {
             throw new NotImplementedException();
@@ -44,6 +48,7 @@ namespace ScannerQR
             btLogout = FindViewById<Button>(Resource.Id.btLogout);
             tbReceiveLocation = FindViewById<EditText>(Resource.Id.tbReceiveLocation);
             tbRealStock = FindViewById<EditText>(Resource.Id.tbRealStock);
+            btConfirm.Click += BtConfirm_Click;
             tbIdent.Enabled = false;
             tbLocation.FocusChange += TbLocation_FocusChange;
             color();
@@ -66,6 +71,99 @@ namespace ScannerQR
             cbWarehouses.ItemSelected += CbWarehouses_ItemSelected;
         }
 
+        private void BtConfirm_Click(object sender, EventArgs e)
+        {
+
+            if (saveHead())
+            {
+
+                try
+                {
+
+                    var headID = moveHead.GetInt("HeadID");
+
+                    string result;
+                    if (WebApp.Get("mode=finish&stock=add&print=" + Services.DeviceUser() + "&id=" + headID.ToString(), out result))
+                    {
+                        if (result.StartsWith("OK!"))
+                        {
+                            var id = result.Split('+')[1];
+                            Toast.MakeText(this, "Zaključevanje uspešno! Št. prevzema:\r\n" + id, ToastLength.Long).Show();
+                            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                            alert.SetTitle("Zaključevanje uspešno");
+                            alert.SetMessage("Zaključevanje uspešno! Št.prevzema:\r\n" + id);
+
+                            alert.SetPositiveButton("Ok", (senderAlert, args) =>
+                            {
+                                alert.Dispose();
+                            });
+
+
+
+                            Dialog dialog = alert.Create();
+                            dialog.Show();
+
+                        }
+                        else
+                        {
+                            Toast.MakeText(this, "Napaka pri zaključevanju: " + result, ToastLength.Long).Show();
+
+                        }
+                    }
+                    else
+                    {
+                        Toast.MakeText(this, "Napaka pri klicu web aplikacije: " + result, ToastLength.Long).Show();
+
+                    }
+                }
+                finally
+                {
+
+                }
+
+
+            }
+        }
+
+
+
+
+        private bool saveHead()
+        {
+           
+       
+            {
+                string error;
+                var currentItem = Services.GetObject("id", dataItem.GetString("Ident"), out error);
+
+                moveItem.SetInt("HeadID", currentItem.GetInt("HeadID"));
+                moveItem.SetString("LinkKey", "");
+                moveItem.SetInt("LinkNo", 0);
+                moveItem.SetString("Ident", tbIdent.Text.Trim());
+                moveItem.SetString("SSCC", tbSSCC.Text.Trim());
+                moveItem.SetString("SerialNo", dataItem.GetString("SerialNo"));
+                moveItem.SetDouble("Packing", Convert.ToDouble(dataItem.GetDouble("RealStock")));
+                moveItem.SetDouble("Qty", Convert.ToDouble(dataItem.GetDouble("RealStock")));
+                moveItem.SetInt("Clerk", Services.UserID());
+                moveItem.SetString("Location", tbLocation.Text.Trim());
+                moveItem.SetString("IssueLocation", dataItem.GetString("Location"));
+
+
+                string error2;
+                moveItem = Services.SetObject("mi", moveItem, out error2); /* Save move item method */
+
+
+                if(moveItem == null)
+                {
+                    Toast.MakeText(this, "Napaka pri dostopu do web aplikacije", ToastLength.Long).Show();
+                    return false;
+                } else
+                {
+                    InUseObjects.Invalidate("MoveItem");
+                    return true;
+                } 
+            }
+        }
         private void TbLocation_FocusChange(object sender, View.FocusChangeEventArgs e)
         {
             ProcessSSCC();
@@ -97,8 +195,8 @@ namespace ScannerQR
 
 
                 string error;
-                var data = Services.GetObject("sscc", tbSSCC.Text, out error);
-                if (data == null)
+                 dataItem = Services.GetObject("sscc", tbSSCC.Text, out error);
+                if (dataItem == null)
                 {
                     Toast.MakeText(this, "Napaka pri preverjanju sscc kode." + error, ToastLength.Long).Show();
 
@@ -107,7 +205,7 @@ namespace ScannerQR
                 }
                 else
                 {
-                    tbIdent.Text = data.GetString("Ident");
+                    tbIdent.Text = dataItem.GetString("Ident");
                   
 
                   
