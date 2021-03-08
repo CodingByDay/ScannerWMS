1a3cfaad3a0c7ac61e02deee2f2c011b0df883ec
Janko <jankojovicic351@gmail.com>
2021-03-08 09:18:23 +0100

    Adding the finished rapi takeover before unit test

--- ScannerQR/RapidTakeover.cs
+++ ScannerQR/RapidTakeover.cs
@@ -4,6 +4,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using ScannerQR.App;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,12 +26,16 @@ namespace ScannerQR
         private Button btLogout;
         private EditText tbIdent;
         private List<ComboBoxItem> data = new List<ComboBoxItem>();
+        private List<TakeOverSerialOrSSCCEntryList> dataX = new List<TakeOverSerialOrSSCCEntryList>();
         private EditText tbReceiveLocation;
         private EditText tbRealStock;
         public static NameValueObject dataItem;
         private NameValueObject moveHead = (NameValueObject)InUseObjects.Get("MoveHead");
         private NameValueObject moveItem = (NameValueObject)InUseObjects.Get("MoveItem");
         private NameValueObject moveItemFinal;
+        private ListView listData;
+        private int selected;
+        private int tempLocation;
 
         public void GetBarcode(string barcode)
         {
@@ -56,6 +61,7 @@ namespace ScannerQR
             tbRealStock.Enabled = false;
             tbIdent.Enabled = false;
             tbLocation.FocusChange += TbLocation_FocusChange;
+            listData = FindViewById<ListView>(Resource.Id.listData);
             color();
 
 
@@ -71,9 +77,48 @@ namespace ScannerQR
             adapterWarehouse.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
             cbWarehouses.Adapter = adapterWarehouse;
 
+            TakeOverSerialOrSSCCEntryAdapter adapter = new TakeOverSerialOrSSCCEntryAdapter(this, dataX);
+            listData.Adapter = adapter;
+
             cbWarehouses.ItemSelected += CbWarehouses_ItemSelected;
+            listData.ItemClick += ListData_ItemClick;
+            
         }
 
+        private void ListData_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            selected = e.Position;
+            var item = dataX.ElementAt(selected);
+            tbLocation.Text = item.Location;
+        }
+
+        private void fillItems()
+        {
+
+            string error;
+            var stock = Services.GetObjectList("str", out error, moveHead.GetString("Wharehouse") + "||" + tbIdent.Text); /* Defined at the beggining of the activity. */
+            var number = stock.Items.Count();
+
+
+            if (stock != null)
+            {
+                stock.Items.ForEach(x =>
+                {
+                    dataX.Add(new TakeOverSerialOrSSCCEntryList // Reusing this type
+                    {
+                        Ident = x.GetString("Ident"),
+                        Location = x.GetString("Location"),
+                        Qty = x.GetDouble("RealStock").ToString(),
+                        SerialNumber = x.GetString("SerialNo")
+
+                    });
+                });
+
+            }
+
+
+
+        }
         private void BtConfirm_Click(object sender, EventArgs e)
         {
 
@@ -131,36 +176,46 @@ namespace ScannerQR
 
         private bool saveHead()
         {
-           
-       
+            var warehouse = data.ElementAt(tempLocation);
+            if (!CommonData.IsValidLocation(warehouse.ID, tbLocation.Text.Trim()))
             {
-                string ssscError;
-                var data = Services.GetObject("sscc", tbSSCC.Text, out ssscError);
-                string error;
+                string WebError = string.Format("Prejemna lokacija" + tbLocation.Text.Trim() + "ni veljavna za sladišće" + moveHead.GetString("Receiver") + "!");
+                Toast.MakeText(this, WebError, ToastLength.Long).Show();
+                tbLocation.RequestFocus();
+                return false;
+            }
+            else
+            {
+
+                {
+                    string ssscError;
+                    var data = Services.GetObject("sscc", tbSSCC.Text, out ssscError);
+                    string error;
 
 
-                if (moveItem == null) { moveItem = new NameValueObject("MoveItem"); }
+                    if (moveItem == null) { moveItem = new NameValueObject("MoveItem"); }
 
-                moveItem.SetInt("HeadID", data.GetInt("HeadID"));
-                moveItem.SetString("LinkKey", data.GetString("LinkKey"));
-                moveItem.SetInt("LinkNo", data.GetInt("LinkNo"));
-                moveItem.SetString("Ident", data.GetString("Ident"));
-                moveItem.SetString("SSCC", data.GetString("SSCC"));
-                moveItem.SetString("SerialNo", data.GetString("SerialNo"));
-                moveItem.SetDouble("Packing", Convert.ToDouble(data.GetDouble("RealStock")));
-                moveItem.SetDouble("Qty", Convert.ToDouble(data.GetDouble("RealStock")));
-                moveItem.SetInt("Clerk", Services.UserID());
-                moveItem.SetString("Location", tbLocation.Text.Trim());
-                moveItem.SetString("IssueLocation", data.GetString("Location"));
+                    moveItem.SetInt("HeadID", data.GetInt("HeadID"));
+                    moveItem.SetString("LinkKey", data.GetString("LinkKey"));
+                    moveItem.SetInt("LinkNo", data.GetInt("LinkNo"));
+                    moveItem.SetString("Ident", data.GetString("Ident"));
+                    moveItem.SetString("SSCC", data.GetString("SSCC"));
+                    moveItem.SetString("SerialNo", data.GetString("SerialNo"));
+                    moveItem.SetDouble("Packing", Convert.ToDouble(data.GetDouble("RealStock")));
+                    moveItem.SetDouble("Qty", Convert.ToDouble(data.GetDouble("RealStock")));
+                    moveItem.SetInt("Clerk", Services.UserID());
+                    moveItem.SetString("Location", tbLocation.Text.Trim());
+                    moveItem.SetString("IssueLocation", data.GetString("Location")); // 
 
-                string error2;
-                moveItemFinal = Services.SetObject("mi", moveItem, out error2); /* Save move item method */
+                    string error2;
+                    moveItemFinal = Services.SetObject("mi", moveItem, out error2); /* Save move item method */
 
-                Toast.MakeText(this, moveItemFinal.ToString(), ToastLength.Long).Show();
-               
+                    Toast.MakeText(this, moveItemFinal.ToString(), ToastLength.Long).Show();
+
                     InUseObjects.Invalidate("MoveItem");
                     return true;
-                }    
+                }
+            }
         }
 
         private void TbLocation_FocusChange(object sender, View.FocusChangeEventArgs e)
@@ -170,11 +225,12 @@ namespace ScannerQR
 
         private void CbWarehouses_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
         {
-           ////////////////
+            tempLocation = e.Position;
         }
 
         private void ProcessSSCC()
         {
+            dataX.Clear();
             var sscc = tbSSCC.Text.Trim();
             if (string.IsNullOrEmpty(sscc)) { return; }
 
@@ -197,7 +253,7 @@ namespace ScannerQR
                     tbIdent.Text = dataItem.GetString("Ident");
                     tbReceiveLocation.Text = dataItem.GetString("Location");
                     tbRealStock.Text = dataItem.GetDouble("RealStock").ToString();
-                                                   
+                    fillItems();
                 }
             
             }
